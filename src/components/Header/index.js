85413d6c9e47d7d3cd4357ac9b6df1b77215258f
DelusionCB <59691622+DelusionCB@users.noreply.github.com>
2020-04-15 09:46:33 +0300

    MUI to Bootstrap
    Header-component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,21 +13,19 @@ import {setLocale as setLocaleAction} from 'src/actions/userLocale'
 import {FormattedMessage} from 'react-intl'
 
 // Material-ui Components
+// import {Button, Drawer, Hidden, makeStyles, Toolbar} from '@material-ui/core'
+// import {Add, Menu, Language, Person} from '@material-ui/icons'
 import Select from 'react-select'
-import {Button, Drawer, Hidden, makeStyles, Toolbar} from '@material-ui/core'
-import {Add, Menu, Language, Person} from '@material-ui/icons'
-
+import {Navbar, Nav, Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom'
 import constants from '../../constants'
 
 import cityOfHelsinkiLogo from '../../assets/images/helsinki-logo.svg'
 import {hasOrganizationWithRegularUsers} from '../../utils/user'
 import {get} from 'lodash'
-import {HelMaterialTheme} from '../../themes/material-ui'
 import {HelSelectTheme, HelLanguageSelectStyles} from '../../themes/react-select'
 import moment from 'moment'
 import * as momentTimezone from 'moment-timezone'
-import helBrandColors from '../../themes/hel/hel-brand-colors'
 
 const {USER_TYPE, APPLICATION_SUPPORT_TRANSLATION} = constants
 
@@ -89,91 +87,65 @@ class HeaderBar extends React.Component {
         const isInsideForm = location.pathname.startsWith('/event/create/new');
 
         return (
-            <div className="main-navbar">
-                <Toolbar className="helsinki-bar">
-                    <div className="helsinki-bar__logo">
-                        <Link to="/">
-                            <img src={cityOfHelsinkiLogo} alt="City Of Helsinki" />
+            <div className='main-navbar'>
+                <Navbar className='bar'>
+                    <div className="bar__logo">
+                        <Link to='/'>
+                            <img src={cityOfHelsinkiLogo} alt='City Logo' />
                         </Link>
                     </div>
-                    <div className="helsinki-bar__login-button">
-                        <div className="helsinki-bar__language-button">
-                            <div className="language-selector">
-                                <Language className="language-icon"/>
-                                <Select
-                                    isClearable={false}
-                                    isSearchable={false}
-                                    value={{
-                                        label: userLocale.locale.toUpperCase(),
-                                        value: userLocale.locale,
-                                    }}
-                                    options={this.getLanguageOptions()}
-                                    onChange={this.changeLanguage}
-                                    styles={HelLanguageSelectStyles}
-                                    theme={HelSelectTheme}
-                                />
-                            </div>  
-                        </div>
-                        {user
-                            ? <Button
-                                style={{color: HelMaterialTheme.palette.primary.contrastText}}
-                                onClick={() => logout()}
-                            >
-                                {user.displayName}
-                            </Button>
-                            : <Button
-                                style={{color: HelMaterialTheme.palette.primary.contrastText}}
-                                startIcon={<Person/>}
-                                onClick={() => login()}
-                            >
-                                <FormattedMessage id="login"/>
-                            </Button>}
-                    </div>
-                </Toolbar>
-                
-                <Toolbar className="linked-events-bar">
-                    <div className="linked-events-bar__logo" onClick={() => routerPush('/')}><FormattedMessage id={`linked-${appSettings.ui_mode}`} /></div>
-                    <div className="linked-events-bar__links">
-                        <Hidden smDown>
-                            <div className="linked-events-bar__links__list">
-                                <NavLinks
-                                    showModerationLink={showModerationLink}
-                                    toMainPage={toMainPage}
-                                    toSearchPage={toSearchPage}
-                                    toHelpPage={toHelpPage}
-                                    toModerationPage={toModerationPage}
-                                />
+                    <Nav className="mx-auto">
+                        <div className='bar__login-button'>
+                            <div className='bar__language-button'>
+                                <div className='language-selector'>
+                                    <Select
+                                        isClearable={false}
+                                        isSearchable={false}
+                                        value={{
+                                            label: userLocale.locale.toUpperCase(),
+                                            value: userLocale.locale,
+                                        }}
+                                        options={this.getLanguageOptions()}
+                                        onChange={this.changeLanguage}
+                                        styles={HelLanguageSelectStyles}
+                                    />
+                                </div>
                             </div>
-                        </Hidden>
-                        <div />
-                        <div className="linked-events-bar__links__mobile">
-                            {!isInsideForm && (
-                                <Button
-                                    variant="outlined"
-                                    className="linked-events-bar__links__create-events"
-                                    onClick={() => routerPush('/event/create/new')}
-                                    startIcon={<Add/>}
-                                >
-                                    <FormattedMessage id={`create-${appSettings.ui_mode}`}/>
+                            {user ? (
+                                <Button className='btnlogin'
+                                   
+                                    onClick={() => logout()}>
+                                    {user.displayName}
+                                </Button>
+                            ) : (
+                                <Button className='btnlogin'
+                                    onClick={() => login()}>
+                                    <FormattedMessage id='login' />
                                 </Button>
                             )}
-                            <Hidden mdUp>
-                                <Menu className="linked-events-bar__icon" onClick={this.toggleNavbar} />
-                                <Drawer anchor='right' open={this.state.navBarOpen} ModalProps={{onBackdropClick: this.toggleNavbar}}>
-                                    <div className="menu-drawer-mobile">
-                                        <NavLinks
-                                            showModerationLink={showModerationLink}
-                                            toMainPage={this.getNavigateMobile(toMainPage)}
-                                            toSearchPage={this.getNavigateMobile(toSearchPage)}
-                                            toHelpPage={this.getNavigateMobile(toHelpPage)}
-                                            toModerationPage={this.getNavigateMobile(toModerationPage)}
-                                        />
-                                    </div> 
-                                </Drawer>
-                            </Hidden>
+                        </div>
+                    </Nav>
+                </Navbar>
+        
+                <Navbar className="linked-events-bar">
+                    <div className="linked-events-bar__links">
+                        <div className="linked-events-bar__links__list">
+                            <NavLinks
+                                showModerationLink={showModerationLink}
+                                toMainPage={toMainPage}
+                                toSearchPage={toSearchPage}
+                                toHelpPage={toHelpPage}
+                                toModerationPage={toModerationPage}
+                            />
+                            {!isInsideForm && (
+                                <Button className='btn'
+                                    onClick={() => routerPush('/event/create/new')}
+                                >
+                                    <FormattedMessage id={`create-${appSettings.ui_mode}`}/>
+                                </Button> )}
                         </div>
                     </div>
-                </Toolbar>
+                </Navbar>
             </div>
         )
     }
@@ -181,9 +153,9 @@ class HeaderBar extends React.Component {
 
 const NavLinks = (props) => {
     const {showModerationLink, toMainPage, toSearchPage, toHelpPage, toModerationPage} = props;
-    const moderationStyles = showModerationLink && makeStyles(theme => ({
-        root: {color: theme.palette.primary.main},
-    }))()
+    const moderationStyles = showModerationLink
+    ()
+    
 
     return (
         <React.Fragment>
