ea09a9485cc14358660d725bf8e10c13e21703fc
DelusionCB <59691622+DelusionCB@users.noreply.github.com>
2020-06-04 12:45:38 +0300

    Changed test name
    Co-Authored-By: Hannes <hienous@users.noreply.github.com>

--- src/components/RecurringEvent/RecurringEvent.test.js
+++ src/components/RecurringEvent/RecurringEvent.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 import RecurringEvent from './index';
 import {IntlProvider, FormattedMessage} from 'react-intl';
 import fiMessages from 'src/i18n/fi.json';
