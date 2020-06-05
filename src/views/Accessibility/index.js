1a9deed6f6de989984013096118ab9af05173f85
Jack Valley <Juuso.laakso@turku.fi>
2020-06-05 13:57:13 +0300

    Added comment

--- src/views/Accessibility/index.js
+++ src/views/Accessibility/index.js
@@ -1,10 +1,10 @@
 import './index.scss';
 
-import {connect} from 'react-redux';
 import React from 'react';
-import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+
+// Accessibility texts fetched from .md files.
 function getContent(language) {
     if (language === 'fi') {
         return require('./content.fi.md');
