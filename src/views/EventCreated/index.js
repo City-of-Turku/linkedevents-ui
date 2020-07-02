9bea2eb0a90f4c0801f775e53998211488a0fe71
tmokko <58773830+tmokko@users.noreply.github.com>
2020-07-02 15:22:53 +0300

    Update index.js
    removed material ui and cleaned code

--- src/views/EventCreated/index.js
+++ src/views/EventCreated/index.js
@@ -2,7 +2,6 @@ import './index.scss'
 
 import React from 'react'
 import {connect} from 'react-redux'
-import {FormattedMessage} from 'react-intl'
 import {push} from 'react-router-redux'
 import PropTypes from 'prop-types'
 
@@ -13,67 +12,18 @@ import CONSTANTS from '../../constants'
 class EventCreated extends React.Component {
 
     componentDidMount() {
-        const {match, setFlashMsg, routerPush} = this.props
+        const {match, routerPush} = this.props
 
         if (match.params.action !== CONSTANTS.EVENT_CREATION.UPDATE) {
-            let headerTranslationId = this.getEventHeaderTranslationId()
-            setFlashMsg(headerTranslationId, CONSTANTS.EVENT_CREATION.SUCCESS)
             routerPush(`/event/${this.props.match.params.eventId}`)
         }
     }
-
-    goToBrowsing() {
-        this.props.routerPush(`/`)
-    }
-
-    getEventHeaderTranslationId() {
-        const EVENT_CREATION = CONSTANTS.EVENT_CREATION
-
-        let headerTranslationId
-
-        switch(this.props.match.params.action) {
-            case EVENT_CREATION.CREATE:
-                headerTranslationId = EVENT_CREATION.CREATE_SUCCESS
-                break;
-            case EVENT_CREATION.UPDATE:
-                headerTranslationId = EVENT_CREATION.UPDATE_SUCCESS
-                break;
-            case EVENT_CREATION.CANCEL:
-                headerTranslationId = EVENT_CREATION.CANCEL_SUCCESS
-                break;
-            case EVENT_CREATION.DELETE:
-                headerTranslationId = EVENT_CREATION.DELETE_SUCCESS
-                break;
-            case EVENT_CREATION.PUBLISH:
-                headerTranslationId = EVENT_CREATION.PUBLISH_SUCCESS
-                break;
-            case EVENT_CREATION.SAVE_DRAFT:
-                headerTranslationId = EVENT_CREATION.SAVE_DRAFT_SUCCESS
-                break;
-            case EVENT_CREATION.SAVE_PUBLIC:
-                headerTranslationId = EVENT_CREATION.SAVE_PUBLIC_SUCCESS
-                break;
-            default:
-                headerTranslationId = EVENT_CREATION.DEFAULT_SUCCESS
-
-        }
-        return headerTranslationId
-    }
-
     render() {
-        if(this.props.match.params.action === 'delete') {
-            let headerTranslationId = this.getEventHeaderTranslationId()
-            return (
-                <div className="event-page">
-                    <h1>
-                        <FormattedMessage id={`${headerTranslationId}`} />
-                    </h1>
-                </div>
-            )
-        }
-        else {
-            return (<div>Loading</div>)
-        }
+        return (
+            <div>
+                <h1>Loading</h1>
+            </div>
+        )
     }
 }
 
@@ -97,6 +47,3 @@ const mapDispatchToProps = (dispatch) => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(EventCreated)
-
-
-
