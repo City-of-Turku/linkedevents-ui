b156cd4478ecb1ddc1bec5bf108c3e89895f93dd
DelusionCB <59691622+DelusionCB@users.noreply.github.com>
2020-08-13 10:44:35 +0300

    Changed legends to headings (h2 & h3)

--- src/components/EventDetails/index.js
+++ src/components/EventDetails/index.js
@@ -137,7 +137,7 @@ const TextValue = (props) => {
 
 const ImageValue = (props) => {
     if (props.value !== undefined && props.value instanceof Object) {
-        return <legend tabIndex='true'><img src={props.value.url} alt={getStringWithLocale(props.value, 'alt_text', props.locale)} className="event-image"/></legend>
+        return <h2><img src={props.value.url} alt={getStringWithLocale(props.value, 'alt_text', props.locale)} className="event-image"/></h2>
     }
     return (
         <FormHeader>
