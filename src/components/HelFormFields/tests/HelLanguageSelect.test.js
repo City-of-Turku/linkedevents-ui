c99a923c02e28ca781e3faee5f89c553508ef1a7
Jack Valley <Juuso.laakso@turku.fi>
2021-03-15 15:19:29 +0200

    Updated tests

--- src/components/HelFormFields/tests/HelLanguageSelect.test.js
+++ src/components/HelFormFields/tests/HelLanguageSelect.test.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import {UnconnectedLanguageSelect} from '../HelLanguageSelect';
 import {FormattedMessage} from 'react-intl';
-import {setLanguages as setLanguageAction} from 'src/actions/editor.js';
-jest.mock('../../../actions/editor');
+import {UnconnectedLanguageSelect} from '../HelLanguageSelect';
 
 const defaultProps = {
-    setLanguages: () => {},
-    onChange: () => {},
     options: [
         {value: 'fi', label: 'FI'},
         {value: 'sv', label: 'SV'},
         {value: 'en', label: 'EN'},
     ],
+    onChange: () => {},
+    setLanguages: () => {},
     checked: ['fi'],
 };
 
-describe('Language Select', () => {
+describe('HelLanguageSelect', () => {
     function getWrapper(props) {
-        return shallow(<UnconnectedLanguageSelect {...props} {...defaultProps} />);
+        return shallow(<UnconnectedLanguageSelect {...defaultProps} {...props} />);
     }
 
     describe('renderer', () => {
+        const availableLanguages = ['fi', 'sv', 'en'];
+
         test('number of divs', () => {
             const div = getWrapper().find('div');
             expect(div).toHaveLength(4);
@@ -30,36 +30,44 @@ describe('Language Select', () => {
         });
         describe('input checkbox', () => {
             test('LanguageSelect input checkbox gets correct props', () => {
-                const checkbox = getWrapper().find('input');
-                expect(checkbox).toHaveLength(3);
-                expect(checkbox.at(0).prop('className')).toBe('custom-control-input');
-                expect(checkbox.at(0).prop('type')).toBe('checkbox');
-                expect(checkbox.at(0).prop('id')).toBe('checkBox-fi');
-                expect(checkbox.at(1).prop('id')).toBe('checkBox-sv');
-                expect(checkbox.at(2).prop('id')).toBe('checkBox-en');
-                expect(checkbox.at(0).prop('aria-checked')).toBe(true);
-                expect(checkbox.at(1).prop('aria-checked')).toBe(false);
-                expect(checkbox.at(2).prop('aria-checked')).toBe(false);
-                expect(checkbox.at(0).prop('aria-disabled')).toBe(true);
-                expect(checkbox.at(1).prop('aria-disabled')).toBe(false);
-                expect(checkbox.at(2).prop('aria-disabled')).toBe(false);
+                const activeLang = availableLanguages[Math.floor(Math.random() * availableLanguages.length)];
+                const checkboxes = getWrapper({checked: [activeLang]}).find('input');
+
+                expect(checkboxes).toHaveLength(3);
+                checkboxes.forEach((checkbox, index) => {
+                    expect(checkbox.prop('className')).toBe('custom-control-input');
+                    expect(checkbox.prop('type')).toBe('checkbox');
+                    expect(checkbox.prop('id')).toBe(`checkBox-${availableLanguages[index]}`);
+                    if (availableLanguages[index] === activeLang) {
+                        expect(checkbox.prop('aria-checked')).toBe(true);
+                        expect(checkbox.prop('aria-disabled')).toBe(true);
+                    } else {
+                        expect(checkbox.prop('aria-checked')).toBe(false);
+                        expect(checkbox.prop('aria-disabled')).toBe(false);
+                    }
+                });
             });
         });
         describe('label and FormattedMessage', () => {
             test('label gets correct props', () => {
-                const label = getWrapper().find('label');
-                expect(label).toHaveLength(3);
-                expect(label.at(0).prop('className')).toBe('custom-control-label disabled');
-                expect(label.at(0).prop('htmlFor')).toBe('checkBox-fi');
-                expect(label.at(1).prop('htmlFor')).toBe('checkBox-sv');
-                expect(label.at(2).prop('htmlFor')).toBe('checkBox-en');
+                const activeLang = availableLanguages[Math.floor(Math.random() * availableLanguages.length)];
+                const labels = getWrapper({checked: [activeLang]}).find('label');
+                expect(labels).toHaveLength(3);
+                labels.forEach((label, index) => {
+                    expect(label.prop('htmlFor')).toBe(`checkBox-${availableLanguages[index]}`);
+                    if (availableLanguages[index] === activeLang) {
+                        expect(label.prop('className')).toBe('custom-control-label disabled');
+                    } else {
+                        expect(label.prop('className')).toBe('custom-control-label');
+                    }
+                });
             });
             test('renders a FormattedMessage', () => {
-                const message = getWrapper().find(FormattedMessage);
-                expect(message).toHaveLength(3);
-                expect(message.at(0).prop('id')).toBe('FI');
-                expect(message.at(1).prop('id')).toBe('SV');
-                expect(message.at(2).prop('id')).toBe('EN');
+                const messages = getWrapper().find(FormattedMessage);
+                expect(messages).toHaveLength(3);
+                messages.forEach((message, index) => {
+                    expect(message.prop('id')).toBe(availableLanguages[index].toUpperCase());
+                });
             });
         });
     });
