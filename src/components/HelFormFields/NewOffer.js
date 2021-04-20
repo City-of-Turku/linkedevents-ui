a0a90bc3fab6903b196c63bcfa2c86408cf62f47
Jack Valley <Juuso.laakso@turku.fi>
2021-04-20 14:27:52 +0300

    Changed string to a number value.

--- src/components/HelFormFields/NewOffer.js
+++ src/components/HelFormFields/NewOffer.js
@@ -92,7 +92,7 @@ class NewOffer extends React.Component {
                 <MultiLanguageField
                     id={'event-price' + this.props.offerKey}
                     type='number'
-                    min='1'
+                    min={1}
                     defaultValue={defaultValue.price} 
                     disabled={isFree} 
                     ref="price" 
